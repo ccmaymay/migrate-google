d124032d752a67d24fadf22567202984c5b21492
Chandler May <chandler.j.may@gmail.com>
2020-05-09 18:25:37 -0400

    Remove duplicates in summarize.

--- summarize-drive-metadata.py
+++ summarize-drive-metadata.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
+import os
 import json
+import time
 from humanfriendly import format_size
-from collections import Counter
+from collections import defaultdict
 
-from migrate_google import configure_logging, LOGGER
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-MISSING_ENTRY_NAME = '???'
+from migrate_google import LOGGER, authenticate, configure_logging
+
+MISSING_ENTRY_NAME = '[???????]'
 
 
 def add_hierarchical_info(root_id, file_metadata_map):
@@ -47,12 +52,23 @@ def add_hierarchical_info(root_id, file_metadata_map):
 def main():
     from argparse import ArgumentParser
     parser = ArgumentParser(description='Load and summarize metadata from drive files')
+    parser.add_argument('credentials_path', help='Path to credentials json file')
+    parser.add_argument('email', help='Email address to use when filtering')
     parser.add_argument('input_path', help='Path to input jsonl metadata file')
     parser.add_argument('--num-top-files', type=int, default=100,
                         help='Number of top (largest) files to show')
+    parser.add_argument('--sleep', type=float,
+                        help='Amount of time to sleep between modifying files')
+    parser.add_argument('--delete-duplicates', action='store_true',
+                        help='Delete all but one copy of each file this email address owns')
     args = parser.parse_args()
 
-    configure_logging()
+    credentials_name = os.path.splitext(os.path.basename(args.credentials_path))[0]
+    configure_logging('summarize-drive-metadata-{}.log'.format(credentials_name))
+
+    creds = authenticate(args.credentials_path, token_path=credentials_name + '.pickle')
+    service = build('drive', 'v3', credentials=creds)
+    files = service.files()
 
     LOGGER.info('Loading data from {}'.format(args.input_path))
     file_metadata_map = dict()
@@ -93,22 +109,38 @@ def main():
             LOGGER.warning('{} parents: {}'.format(
                 len(parents), file_metadata_map[file_id]['path']))
 
-    LOGGER.info('Checking for duplicate directory entries')
-    for (file_id, file_metadata) in file_metadata_map.items():
-        child_counts = Counter(
-            file_metadata_map[child_id]['name']
-            for child_id
-            in file_metadata['children'])
-        for (name, count) in child_counts.most_common():
-            if count == 1:
-                break
-            child_ids = [
-                file_metadata_map[child_id]['id']
-                for child_id
-                in file_metadata['children']
-                if file_metadata_map[child_id]['name'] == name]
-            LOGGER.warning('Duplicate entry: {}'.format(
-                file_metadata_map[child_ids[0]]['path']))
+    LOGGER.info('Checking for duplicate entries')
+    file_counts = defaultdict(set)
+    for (file_id, m) in file_metadata_map.items():
+        file_counts[(
+            m['name'],
+            m['path'],
+            tuple(sorted(m['parents'])),
+            m['size'],
+            tuple(sorted(
+                (
+                    p['type'],
+                    p.get('role'),
+                    p.get('emailAddress'),
+                ) for p in m.get('permissions', [])
+            )),
+        )].add(file_id)
+    for (t, file_ids) in file_counts.items():
+        if len(file_ids) > 1 and t[0] != MISSING_ENTRY_NAME and ('user', 'owner', args.email) in t[-1]:
+            LOGGER.warning('{} copies of entry: {}'.format(
+                len(file_ids), t[1]))
+            if args.delete_duplicates:
+                try:
+                    retrieved_file_ids = [
+                        files.get(fileId=file_id).execute()['id'] for file_id in file_ids]
+                    if sorted(retrieved_file_ids) == sorted(file_ids):
+                        for file_id in retrieved_file_ids[1:]:
+                            LOGGER.warning('Deleting {}'.format(file_id))
+                            files.delete(fileId=file_id)
+                except HttpError as ex:
+                    LOGGER.warning('Caught exception: {}'.format(ex))
+
+                time.sleep(args.sleep)
 
     LOGGER.info('Listing {} largest files by size'.format(args.num_top_files))
     file_ids_by_size = sorted(
